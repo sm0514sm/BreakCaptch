493796d47a2e51cd1a6bd49b9d13e5c0879af008
lifaon <sm0514sm@gmail.com>
2019-03-28 14:30:07 +0900

    휴대폰 본인확인 페이지 크롤링 시작

--- OpenChromeCrawling.py
+++ OpenChromeCrawling.py
@@ -22,6 +22,42 @@ def input_user_info(element_name, value):
 
 def input_Gmarket_user_info():
     print("input_Gmarket_user_info() 실행")
+
+    # try:
+    #     # TODO 여기서 속도가 매우 느려지는데 멀티 프로세싱을 통해 단축 시킬 수 있다.
+    #     iframe = driver.find_element_by_xpath("//div[@id='GmktPopLayer']/div[@id='popLayer1']/"
+    #                                           "div[@id='popLayerContents1']/iframe[@name='popLayerIframe1']")
+    #     driver.switch_to.frame(iframe)
+    # except:
+    #     print("iframe 이 존재하지 않거나 접근할 수 없음")
+    #     return
+    #
+    # print("user_info 입력")
+    #
+    # if not input_user_info('u_name', user_info["이름"]):
+    #     print("이름 입력 실패")
+    #
+    # select = Select(driver.find_element_by_name('naSelect'))
+    # select.select_by_visible_text(user_info["국적"])
+    #
+    # if not input_user_info('birth_date', user_info["생년월일"]):
+    #     print("생년월일 입력 실패")
+    #
+    # print("성별 선택")
+    # if user_info["성별"] == "남자":
+    #     driver.find_element_by_id("gender_male").click()
+    # else:
+    #     driver.find_element_by_id("gender_female").click()
+    #
+    # select = Select(driver.find_element_by_name('carrier_sel'))
+    # select.select_by_visible_text(user_info["통신사"])
+    #
+    # if not input_user_info('cellphone_num', user_info["휴대폰번호"]):
+    #     print("휴대폰 번호 입력 실패")
+
+
+def input_cellphone_user_info():
+    print("input_cellphone_user_info() 실행")
     try:
         # TODO 여기서 속도가 매우 느려지는데 멀티 프로세싱을 통해 단축 시킬 수 있다.
         iframe = driver.find_element_by_xpath("//div[@id='GmktPopLayer']/div[@id='popLayer1']/"
@@ -109,10 +145,15 @@ def do_crawling():
         #    "https://sslmember2.gmarket.co.kr/FindID/FindID?targetUrl=http%3a%2f%2fwww.gmarket.co.kr%2f%3fredirect%3d1")
         while True:
             time.sleep(1)
+            driver.switch_to.window(driver.window_handles[-1])
             url = driver.current_url
-            print(driver.current_url)
+            print(url)
             if "sslmember2.gmarket.co.kr/FindID" in url:
                 input_Gmarket_user_info()
+            elif "mobile-ok.com/SimplePop" in url:
+                input_cellphone_user_info()
+
+
     except:
         print("크롬 드라이버를 찾을 수 없음")
         exit()
