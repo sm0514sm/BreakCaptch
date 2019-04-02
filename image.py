4ffbe1d5193fa2988a76c3c9b0b467e28670d7d2
이상민 (Onz) <sm0514sm@ajou.ac.kr>
2019-04-02 18:49:23 +0900

    코드 리팩토링

--- image.py
+++ image.py
@@ -1,141 +1,103 @@
-from PIL import Image, ImageFilter
-# TODO 코드 리팩토링
+from PIL import Image
 
-file_name = "CurrentImage (10)"
-lists = []
+
+CLEAR = (0, 0, 0, 0)            # 투명
+WHITE = (255, 255, 255, 255)    # 흰색
+BLACK = (0, 0, 0, 255)          # 검정
+
+
+# TODO 각 함수별로 에러처리 필요
+
+
+# 블러 픽셀 제거 (alpha 값이 255가 아니면 삭제)
+def blur_pixel_delete(image, pixel):
+    for y in range(image.height):
+        for x in range(image.width):
+            if pixel[x, y][3] != 255:
+                pixel[x, y] = CLEAR
+    image.save("./image_test/" + file_name + "_0_blur_delete.png")
+
+
+# 라인 삭제하기 (blur_pixel_delete 미리 해야함)
+def line_delete(image, pixel):
+    spe_h = 0
+
+    # 다섯가지 색상만이 존재하는 special_height 구하기
+    for y in range(image.height):
+        global color_lists
+        color_lists = []
+        for x in range(image.width):
+            if pixel[x, y] not in color_lists:
+                if pixel[x, y] != CLEAR and\
+                        (pixel[x, y] == pixel[x+2, y] or pixel[x, y] == pixel[x-2, y]):
+                    color_lists.append(pixel[x, y])
+        if len(color_lists) == 5:
+            spe_h = y
+            print("special_height : %d" % y)
+            break
+
+    # color_lists에 없는 색상 삭제, 있는 색상은 각 색상 별 최소 x 위치
+    for y in range(image.height):
+        for x in range(image.width):
+            if px[x, y] not in color_lists:     # 색상 리스트에 없으면 해당 색상은 삭제
+                px[x, y] = CLEAR
+            else:                               # 색상 리스트에 있으면 해당 위치와 각 색깔 별 최소 x위치 판별
+                for i in range(len(color_lists)):
+                    if px[x, y] == color_lists[i]:
+                        color_most_left_list[i] = min(color_most_left_list[i], x)
+    im.save("./image_test/" + file_name + "_1_line_delete.png")
+
+    return spe_h
+
+
+# 문자 색상별로 5개로 나누기
+# bbox = (x, y, width+x, height+y)
+# spe_h = special_height
+def character_separate(image, bbox, spe_h, naming):
+    new_image = image.crop(bbox)
+    new_pixel = new_image.load()
+
+    # TODO bbox 범위 초과시 에러 출력 종료
+
+    # 자른 이미지 글자의 주 색상 찾기
+    this_image_color = ()
+    for x in range(new_image.width):
+        if new_pixel[x, spe_h] != CLEAR:
+            this_image_color = new_pixel[x, spe_h]  # TODO 지금 이 방식 문제 있음, 가장 많은 색을 추출하는 식으로 해야할듯, 반례 CurrentImage (8)
+            print(naming, "색상 :", this_image_color)
+            break
+
+    # 해당 색상은 검정색으로, 나머지는 흰색으로
+    for y in range(new_image.height):
+        for x in range(new_image.width):
+            if new_pixel[x, y] != this_image_color:
+                new_pixel[x, y] = WHITE
+            else:
+                new_pixel[x, y] = BLACK
+
+    new_image.save("./image_test/" + file_name + "_result_" + naming + ".png")
+
+
+# ------------------------------------------------------------------------------------------------------------------- #
+
+
+file_name = "CurrentImage (7)"
+color_lists = []
 im = Image.open(file_name + '.png')
-print("width : %d, height : %d" % (im.width, im.height))
+print("width  : %d\nheight : %3d" % (im.width, im.height))
 color_most_left_list = [im.width, im.width, im.width, im.width, im.width]
 px = im.load()
-for i in range(im.height):
-    for j in range(im.width):
-        if px[j, i][3] != 255:
-            px[j, i] = (0, 0, 0, 0,)
-# 블러 이미지 삭제
-im.save("./image_test/" + file_name + "_0_blur_delete.png")
 
-# 라인 삭제하기
-seth = 0
-for tmp_h in range(im.height):
-    tmp_lists = []
-    for tmp_w in range(im.width):
-        if px[tmp_w, tmp_h] not in tmp_lists:
-            if px[tmp_w, tmp_h] != (0, 0, 0, 0) and\
-                    (px[tmp_w, tmp_h] == px[tmp_w+2, tmp_h] or px[tmp_w, tmp_h] == px[tmp_w-2, tmp_h]):
-                tmp_lists.append(px[tmp_w, tmp_h])
-    if len(tmp_lists) == 5:
-        print("set height : %d" % tmp_h)
-        print(tmp_lists)
-        seth = tmp_h
-        lists = tmp_lists
-        break
+blur_pixel_delete(im, px)
 
-for i in range(im.height):
-    for j in range(im.width):
-        if px[j, i] not in lists:   # 색상 리스트에 없으면 해당 색상은 삭제
-            px[j, i] = (0, 0, 0, 0)
-        else:                       # 색상 리스트에 있으면 해당 위치와 각 색깔 별 최소 x위치 판별
-            for k in range(len(lists)):
-                if px[j, i] == lists[k]:
-                    color_most_left_list[k] = min(color_most_left_list[k], j)
-im.save("./image_test/" + file_name + "_1_line_delete.png")
-print(color_most_left_list)
+special_height = line_delete(im, px)
+print("색상 별 최소 x값 : ", color_most_left_list)
 
-# color_most_left_list 기준으로 숫자들 나누기
-new0im = im.crop((color_most_left_list[0], 0, color_most_left_list[0] + 45, 45))
-px0 = new0im.load()
-new1im = im.crop((color_most_left_list[1], 0, color_most_left_list[1] + 45, 45))
-px1 = new1im.load()
-new2im = im.crop((color_most_left_list[2], 0, color_most_left_list[2] + 45, 45))
-px2 = new2im.load()
-new3im = im.crop((color_most_left_list[3], 0, color_most_left_list[3] + 45, 45))
-px3 = new3im.load()
-new4im = im.crop((color_most_left_list[4], 0, color_most_left_list[4] + 45, 45))
-px4 = new4im.load()
+num = 0
+for most_left in color_most_left_list:
+    character_separate(im, (most_left, 0, most_left + 60, 45), special_height, str(num))
+    num += 1
 
-this_image_color = ()
-for j in range(45):
-    if px0[j, seth] != (0, 0, 0, 0):
-        this_image_color = px0[j, seth]  # 지금 이 방식 문제 있음, 가장 많은 색을 추출하는 식으로 해야할듯 # 반례 CurrentImage (8)
-        print(this_image_color)
-        break
-for i in range(45):
-    for j in range(45):
-        if px0[j, i] != this_image_color:
-            px0[j, i] = (255, 255, 255, 255)
-        else:
-            px0[j, i] = (0, 0, 0, 255)
-
-
-this_image_color = ()
-for j in range(45):
-    if px1[j, seth] != (0, 0, 0, 0):
-        this_image_color = px1[j, seth]
-        print(this_image_color)
-        break
-for i in range(45):
-    for j in range(45):
-        if px1[j, i] != this_image_color:
-            px1[j, i] = (255, 255, 255, 255)
-        else:
-            px1[j, i] = (0, 0, 0, 255)
-
-this_image_color = ()
-for j in range(45):
-    if px2[j, seth] != (0, 0, 0, 0):
-        this_image_color = px2[j, seth]
-        print(this_image_color)
-        break
-for i in range(45):
-    for j in range(45):
-        if px2[j, i] != this_image_color:
-            px2[j, i] = (255, 255, 255, 255)
-        else:
-            px2[j, i] = (0, 0, 0, 255)
-
-this_image_color = ()
-for j in range(45):
-    if px3[j, seth] != (0, 0, 0, 0):
-        this_image_color = px3[j, seth]
-        print(this_image_color)
-        break
-for i in range(45):
-    for j in range(45):
-        if px3[j, i] != this_image_color:
-            px3[j, i] = (255, 255, 255, 255)
-        else:
-            px3[j, i] = (0, 0, 0, 255)
-
-this_image_color = ()
-for j in range(45):
-    if px4[j, seth] != (0, 0, 0, 0):
-        this_image_color = px4[j, seth]
-        break
-for i in range(45):
-    for j in range(45):
-        if px4[j, i] != this_image_color:
-            px4[j, i] = (255, 255, 255, 255)
-        else:
-            px4[j, i] = (0, 0, 0, 255)
-
-
-# 이미지 흑백으로 변경
-for i in range(im.height):
-    for j in range(im.width):
-        if px[j, i] == (0, 0, 0, 0):
-            px[j, i] = (255, 255, 255, 255)
-
-for i in range(im.height):
-    for j in range(im.width):
-        if px[j, i] != (255, 255, 255, 255):
-            px[j, i] = (0, 0, 0, 255)
-
-im.save("./image_test/" + file_name + "_2_make_blackwhite.png")
-if len(lists) != 5:
+if len(color_lists) != 5:
     print("비정상 작동")
 
-new0im.save("./image_test/" + file_name + "_result_0.png")
-new1im.save("./image_test/" + file_name + "_result_1.png")
-new2im.save("./image_test/" + file_name + "_result_2.png")
-new3im.save("./image_test/" + file_name + "_result_3.png")
-new4im.save("./image_test/" + file_name + "_result_4.png")
