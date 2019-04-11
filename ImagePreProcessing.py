192526cdaa2acff1be85bc691db1bacfdcfdedf3
이상민 (Onz) <sm0514sm@ajou.ac.kr>
2019-04-11 15:14:55 +0900

    파일 저장할 때 train/test 분류 기능 추가

--- ImagePreProcessing.py
+++ ImagePreProcessing.py
@@ -7,6 +7,7 @@ WHITE = (255, 255, 255, 255)    # 흰색
 BLACK = (0, 0, 0, 255)          # 검정
 
 MAX_WIDTH = 60
+TRAIN_NUM = 2   # 주의 : 5개밖에 없는 문자있음
 
 
 # 블러 픽셀 제거 (alpha 값이 255가 아니면 삭제)
@@ -85,9 +86,19 @@ def character_separate(image, bbox, naming, char, save_dir_name):
     new_image = bg_image
     if not os.access(path_dir + save_dir_name, os.F_OK):
         os.mkdir(path_dir + save_dir_name)
-    if not os.access(path_dir + save_dir_name + "/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
-        os.mkdir(path_dir + save_dir_name + "/" + char)
-    new_image.save(path_dir + save_dir_name + "/" + char + "/" + file_name[:5] + "_result_" + naming + ".jpg")
+    if not os.access(path_dir + save_dir_name + "/train/", os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/train/")
+    if not os.access(path_dir + save_dir_name + "/test/", os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/test/")
+    if not os.access(path_dir + save_dir_name + "/train/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/train/" + char)
+    if not os.access(path_dir + save_dir_name + "/test/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/test/" + char)
+    if Black_no_line_counter[ord(char)-65] == TRAIN_NUM:
+        new_image.save(path_dir + save_dir_name + "/train/" + char + "/" + file_name[:5] + "_result_" + naming + ".png")
+    else:
+        new_image.save(path_dir + save_dir_name + "/test/" + char + "/" + file_name[:5] + "_result_" + naming + ".png")
+        Black_no_line_counter[ord(char) - 65] += 1
 
 
 # 칼라 + 크기일정 + 라인유지
@@ -106,9 +117,19 @@ def character_separate_with_line(image, bbox, naming, char, save_dir_name):
 
     if not os.access(path_dir + save_dir_name, os.F_OK):
         os.mkdir(path_dir + save_dir_name)
-    if not os.access(path_dir + save_dir_name + "/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
-        os.mkdir(path_dir + save_dir_name + "/" + char)
-    new_image.save(path_dir + save_dir_name + "/" + char + "/" + file_name[:5] + "_result_" + naming + ".jpg")
+    if not os.access(path_dir + save_dir_name + "/train/", os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/train/")
+    if not os.access(path_dir + save_dir_name + "/test/", os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/test/")
+    if not os.access(path_dir + save_dir_name + "/train/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/train/" + char)
+    if not os.access(path_dir + save_dir_name + "/test/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/test/" + char)
+    if Color_with_line_counter[ord(char) - 65] == TRAIN_NUM:
+        new_image.save(path_dir + save_dir_name + "/train/" + char + "/" + file_name[:5] + "_result_" + naming + ".png")
+    else:
+        new_image.save(path_dir + save_dir_name + "/test/" + char + "/" + file_name[:5] + "_result_" + naming + ".png")
+        Color_with_line_counter[ord(char) - 65] += 1
 
 
 # 칼라 + 크기일정 + 라인삭제
@@ -145,9 +166,19 @@ def character_separate_color_no_line(image, bbox, naming, char, save_dir_name):
 
     if not os.access(path_dir + save_dir_name, os.F_OK):
         os.mkdir(path_dir + save_dir_name)
-    if not os.access(path_dir + save_dir_name + "/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
-        os.mkdir(path_dir + save_dir_name + "/" + char)
-    new_image.save(path_dir + save_dir_name + "/" + char + "/" + file_name[:5] + "_result_" + naming + ".jpg")
+    if not os.access(path_dir + save_dir_name + "/train/", os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/train/")
+    if not os.access(path_dir + save_dir_name + "/test/", os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/test/")
+    if not os.access(path_dir + save_dir_name + "/train/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/train/" + char)
+    if not os.access(path_dir + save_dir_name + "/test/" + char, os.F_OK):  # 해당 디렉토리가 이미 존재하는지 확인
+        os.mkdir(path_dir + save_dir_name + "/test/" + char)
+    if Color_no_line_counter[ord(char) - 65] == TRAIN_NUM:
+        new_image.save(path_dir + save_dir_name + "/train/" + char + "/" + file_name[:5] + "_result_" + naming + ".png")
+    else:
+        new_image.save(path_dir + save_dir_name + "/test/" + char + "/" + file_name[:5] + "_result_" + naming + ".png")
+        Color_no_line_counter[ord(char) - 65] += 1
 
 
 # ------------------------------------------------------------------------------------------------------------------- #
@@ -155,52 +186,37 @@ def character_separate_color_no_line(image, bbox, naming, char, save_dir_name):
 if __name__ == '__main__':
     path_dir = "./보안문자/"     # >> 이미지 파일들이 있는 디렉토리
     save_dir = ""               # >> 수정금지
-    color_most_left_list = [999 for i in range(5)]
-    color_most_right_list = [0 for i in range(5)]
+
+    Color_with_line_counter = [0 for a in range(26)]
+    Color_no_line_counter = [0 for a in range(26)]
+    Black_no_line_counter = [0 for a in range(26)]
 
     file_list = os.listdir(path_dir)
     file_list.sort()
-    print("Black no_line 생성 중")
-    print("Color no_line 생성 중")
     for file_name in file_list:
+        color_most_left_list = [999 for i in range(5)]
+        color_most_right_list = [0 for i in range(5)]
         color_lists = []    # 해당 이미지의 5가지 문자 각각의 색 (R, G, B, A) 리스트
 
         if os.path.isdir(path_dir + file_name):  # 디렉토리일 경우 넘김
             continue
         im = Image.open(path_dir + file_name)
+        line_im = Image.open(path_dir + file_name)
         px = im.load()
 
         blur_pixel_delete(im, px)
+        blur_pixel_delete(line_im, px)
         special_height = line_delete(im, px)
 
         num = 0
         for num in range(5):
+            character_separate_with_line(line_im, (color_most_left_list[num], 0, color_most_right_list[num], 45),
+                                         str(num), file_name[num], "Color with_line")
             character_separate_color_no_line(im, (color_most_left_list[num], 0, color_most_right_list[num], 45),
                                              str(num), file_name[num], "Color no_line")
             character_separate(im, (color_most_left_list[num], 0, color_most_right_list[num], 45),
                                str(num), file_name[num], "Black no_line")
             num += 1
-
-    # ------------------------------------------------------------------ #
-    file_list2 = os.listdir(path_dir)
-    file_list2.sort()
-    print("Color with_line 생성 중")
-    for file_name in file_list2:
-        color_lists = []  # 해당 이미지의 5가지 문자 각각의 색 (R, G, B, A) 리스트
-
-        if os.path.isdir(path_dir + file_name):  # 디렉토리일 경우 넘김
-            continue
-        im = Image.open(path_dir + file_name)
-        px = im.load()
-
-        blur_pixel_delete(im, px)
-
-        num = 0
-        for num in range(5):
-            character_separate_with_line(im, (color_most_left_list[num], 0, color_most_right_list[num], 45),
-                                         str(num), file_name[num], "Color with_line")
-            num += 1
-
     print("""
     ----------------------------
     Done""")
