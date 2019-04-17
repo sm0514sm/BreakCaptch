68d5b26d17fd35796c364a125b775c9fe3131091
KiaraR <hcho2256@ajou.ac.kr>
2019-04-17 16:19:46 +0900

    송연주: 음성 0기준으로 자르기 완료 (디버깅까지 완료)

--- AudioRecongization.py
+++ AudioRecongization.py
@@ -5,8 +5,8 @@ import sys
 import numpy as np
 from scipy.io.wavfile import read
 
-MAX_SIZE = 11000
-NULL_SIZE = 3451
+MAX_SIZE = 7000
+NULL_SIZE = 3450
 
 def audio_cut():
     # 함수로 만들어서 다른 파일들이랑 이어붙여야함
@@ -40,76 +40,47 @@ def padding_audio():
     anp = np.array(a[1], dtype=int)
     print(anp)
     print(anp.size)
-    pad = np.zeros(11000-anp.size)
+    pad = np.zeros(MAX_SIZE-anp.size)
     first = np.append(anp, pad)
     print(first.size)
     np.savetxt('./' + audio + '.csv', first.T, header='Z', fmt='%d')  # 횡으로 저장하는방법 모르게쑴
 
 def cut_by_null():
-    audio = "ABFDO"
+    audio = "NJWJA"
     # 음성 파일 가져오기 <- 원래는 웹페이지에서 가져와야함.
 
     a = read('audio/' + audio + '.wav')
     anp = np.array(a[1], dtype=int)
     print(anp.size)
-    cnt = 0
-    i = NULL_SIZE
+    np.savetxt("./" + audio + ".csv", anp.T, fmt='%d')
+    cnt = -1
+    null_count = np.arange(0)
+    i = 0
     alpha = np.arange(0)
     # 0 자르기
     while i < anp.size:
-        if anp[i] == 0 and anp[i + 1] == 0:
-            i += 3451
-            pad = np.zeros(MAX_SIZE - alpha.size)
-            alpha = np.append(alpha, pad)
-            np.savetxt("./" + audio[cnt] + ".csv", alpha.T, header=audio[cnt], fmt='%d')
-            alpha = np.arange(0)
-            cnt += 1
+        if anp[i] == 0:
+            while i < anp.size and anp[i] == 0:
+                null_count = np.append(null_count, anp[i])
+                i += 1
+            if null_count.size < 10 : alpha = np.append(alpha, null_count) #중간의 0
+            else : #빈공간 0
+                pad = np.zeros(MAX_SIZE - alpha.size)
+                alpha = np.append(alpha, pad)
+                if cnt in range(0,4): np.savetxt("./" + audio[cnt] + ".csv", alpha.T, header=audio[cnt], fmt='%d')
+                print(audio[cnt], cnt, alpha.size, null_count.size, i, anp.size)
+                alpha = np.arange(0)
+                cnt += 1
+            null_count = np.arange(0)
         else:
-            print('[', i, ']    ', anp[i])
+           # print('[', i, ']    ', anp[i])
             alpha = np.append(alpha,anp[i])
             # first = np.array(anp[i:i+ 11000], dtype=int)
             i += 1
+    print(audio[cnt], cnt, alpha.size, null_count.size, i, anp.size)
+    np.savetxt("./" + audio[cnt] + ".csv", alpha.T, header=audio[cnt], fmt='%d')
 
 
 if __name__ == '__main__':
     # audio_cut()
     cut_by_null()
-
-#폴더 생성
-# dir = audio + "/"
-# if not os.path.isdir(dir):
-#     os.mkdir(dir)
-#     음성 파일 생성
-    # first_alphabet.export(dir + "1.wav", format="wav")
-    # second_alphabet.export(dir + "2.wav", format="wav")
-    # third_alphabet.export(dir + "3.wav", format="wav")
-    # fourth_alphabet.export(dir + "4.wav", format="wav")
-    # fifth_alphabet.export(dir + "5.wav", format="wav")
-#
-# print(first_alphabet.duration_seconds)
-
-# import librosa
-# import librosa.display
-# import IPython.display
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import matplotlib as mpl
-# import matplotlib.font_manager as fm
-#
-# y, sr = librosa.load('CKPJR.wav')
-# IPython.display.Audio(data=y, rate=sr)
-#
-# D = librosa.amplitude_to_db(np.abs(librosa.stft(y[:1024])), ref=np.max)
-#
-# plt.plot(D.flatten())
-# plt.show()
-#
-# S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
-#
-# log_S = librosa.logamplitude(S, ref_power=np.max)
-# plt.figure(figsize=(12,4))
-# librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
-# plt.title('mel power spectrogram')
-# plt.colorbar(format='%+02.0f dB')
-# plt.tight_layout()
-# plt.show()
